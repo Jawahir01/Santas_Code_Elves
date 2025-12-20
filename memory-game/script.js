d9ef9ff2f48c0e315861289745e0880ff6ee51d2
Colm Woods <colmwoods13@gmail.com>
2025-12-20 12:28:04 +0000

    Add lockBoard = false to prevent users from selecting a third card

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,2 +1,6 @@
 const cards = document.querySelectorAll(".memory-card");
 const restartBtn = document.getElementById("restart");
+
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
